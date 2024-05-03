8cf0c6a7d71de30d2c08f1e71d003819b6f7794f
Refodeus <Romylik22R@yandex.ru>
2024-05-03 22:13:53 +0300

    I did panel with a text

--- MagicBookProject/PlayPage.xaml.cs
+++ MagicBookProject/PlayPage.xaml.cs
@@ -9,4 +9,22 @@ public partial class PlayPage : ContentPage
 		InitializeComponent();
 		BindingContext = vm;
 	}
+	private async void AnimatedText(Label text)
+	{
+		string downloadText = "Я тот самый человек, который лицезрел мироздания во время величия хаоса на Земле";
+		text.Text = string.Empty;
+		foreach(char c in downloadText)
+		{
+			text.Text += c;
+			await Task.Delay(100);
+		}
+	}
+	private void OnImageTapped(object sender, EventArgs e)
+	{
+		if (sender is Image image)
+		{
+			var text = (Label)image.Parent.FindByName("MainText");
+			AnimatedText(text);
+		}
+	}
 }
