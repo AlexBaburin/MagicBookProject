eaa7b380b5a9b994e3c82395511aa8ff23604080
Refodeus <Romylik22R@yandex.ru>
2024-05-03 23:21:53 +0300

    fixed bug with text

--- MagicBookProject/PlayPage.xaml.cs
+++ MagicBookProject/PlayPage.xaml.cs
@@ -4,6 +4,7 @@ namespace MagicBookProject;
 
 public partial class PlayPage : ContentPage
 {
+	bool lever = true;
 	public PlayPage(PlayViewModel vm)
 	{
 		InitializeComponent();
@@ -12,13 +13,19 @@ public partial class PlayPage : ContentPage
 	private async void AnimatedText(Label text)
 	{
 		string downloadText = "Я тот самый человек, который лицезрел мирозданиe во время величия хаоса на Земле";
-		text.Text = string.Empty;
-		foreach(char c in downloadText)
+		if (lever)
 		{
-			text.Text += c;
-			await Task.Delay(100);
-		}
-	}
+			text.Text = string.Empty;
+            foreach (char c in downloadText)
+            {
+                if (lever)
+                    lever = false;
+                text.Text += c;
+                await Task.Delay(100);
+            }
+            lever = true;
+        }
+    }
 	private void OnImageTapped(object sender, EventArgs e)
 	{
 		if (sender is Image image)
